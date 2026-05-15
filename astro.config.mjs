258c230062393ebd2494fe7b9cc2905ecbaf5b5c
ruabien <ruabien1504@gmail.com>
2026-05-15 20:17:05 +0700

    Update astro.config.mjs
    Fix icon and alias v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import icon from 'astro-icon'; 
+import icon from 'astro-icon';
 
 export default defineConfig({
-  // Đăng ký các thành phần mở rộng
   integrations: [
     mdx(), 
     sitemap(), 
-    icon() // Thêm dòng này để giải quyết lỗi virtual:astro-icon
+    icon()
   ],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
-      alias: [
-        { find: '@', replacement: '/src' }
-      ],
+      alias: {
+        '@': '/src',
+      },
     },
   },
 });
