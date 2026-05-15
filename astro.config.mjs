d661f5c7c1654aeb415969e3d05d57f01b3e75e9
ruabien <ruabien1504@gmail.com>
2026-05-15 19:58:12 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   integrations: [mdx(), sitemap()],
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
-        '@': path.resolve('./src'),
+        '@': '/src',
       },
     },
   },
