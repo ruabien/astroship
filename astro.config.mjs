abb306eb1debd7d1f9d05be2da5af054535e1cba
ruabien <ruabien1504@gmail.com>
2026-05-15 20:55:38 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import icon from 'astro-icon';
+import icon from 'astro-icon'; // PHẢI CÓ DÒNG NÀY
 
 export default defineConfig({
   integrations: [
     mdx(), 
     sitemap(), 
-    icon()
+    icon() // PHẢI CÓ DÒNG NÀY
   ],
   vite: {
     plugins: [tailwindcss()],
