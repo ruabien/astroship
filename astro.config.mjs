eb34e962caaa4a8fd34cecc81a504dce33fda7e6
ruabien <ruabien1504@gmail.com>
2026-05-15 21:11:32 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import icon from 'astro-icon'; // PHẢI CÓ DÒNG NÀY
+import icon from 'astro-icon';
 
 export default defineConfig({
+  // THÊM DÒNG NÀY (thay bằng link .pages.dev của bạn nếu có, hoặc để tạm link ảo)
+  site: 'https://astroship.pages.dev', 
+  
   integrations: [
     mdx(), 
     sitemap(), 
-    icon() // PHẢI CÓ DÒNG NÀY
+    icon()
   ],
   vite: {
     plugins: [tailwindcss()],
