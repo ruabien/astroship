b8807fa9be029fc7ce9e3b6422733fc291722c8f
ruabien <ruabien1504@gmail.com>
2026-05-15 19:53:30 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import path from 'node:path';
 
 export default defineConfig({
   integrations: [mdx(), sitemap()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
