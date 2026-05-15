346aa2c21811aa37df270d47ac372514fabe530a
ruabien <ruabien1504@gmail.com>
2026-05-15 20:08:03 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon'; // Thêm dòng này
 
 export default defineConfig({
-  integrations: [mdx(), sitemap()],
+  // Tích hợp các công cụ vào Astro
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    icon() // Thêm dòng này để sửa lỗi virtual:astro-icon
+  ],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
