c224422d6fb722209bd82c057f37d206eee5803a
ruabien <ruabien1504@gmail.com>
2026-05-16 16:21:42 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 import cloudflare from '@astrojs/cloudflare'; // 1. Thêm lại dòng này
 
 export default defineConfig({
-  site: 'https://astroship-cuv.pages.dev', // Thay bằng link thật của bạn
+  site: 'https://hotro.online', // Thay bằng link thật của bạn
   
   output: 'server', // 2. Chuyển sang chế độ Server để làm trang bán hàng
   adapter: cloudflare({
